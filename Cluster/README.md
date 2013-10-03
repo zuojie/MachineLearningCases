ba0250a45d810610fc22c48b1371524740930f73
zuojie <jean@ritekiMacBook-Air.local>
2013-10-03 10:09:31 -0700

    ..

---
+++ Cluster/README.md
@@ -0,0 +1,18 @@
+##### By means of clustering algorithm, I have clustered my *weibo* following guys. Sorry for the invasion of privacy :P.   
+* clusters.py is the clustering algorithm lib, including function to visualitions of the cluster result
+* get_weibo_dat.py is the crawler to get the weibo user's info data
+* weibo_cluster.py is responsible for preprocessing the data and invoking clustering function and drawing dendrogram.
+* source data is inclusive under data directory 
+* weibo_cluster.jpg is the clustering result
+
+___
+
+##### 利用聚类算法（分级聚类），对我的微博关注人进行了一个简单聚类, 冒犯之处，多多海涵。   
+* cluster.py 是聚类算法库和聚类结果可视化的函数
+* get_weibo_dat.py 负责抓取微博用户数据
+* weibo_cluster.py负责预处理数据，调用聚类算法进行聚类，然后绘制出图
+* 源数据放在data目录下
+* weibo_cluster.jpg即为最终聚类的结果输出
+
+#####Appendix
+![weibo cluster](http://zuojie.github.io/demo/ml_1.jpg)
--- Cluster/weibo_cluster.py
+++ Cluster/weibo_cluster.py
@@ -2,5 +2,49 @@
 # author: zuojiepeng
 from clusters import *
 
+def Tanimoto(v1, v2):
+	c1, c2, shr = 0, 0, 0
+	for i in range(len(v1)):
+		if v1[i] != 0: c1 += 1
+		if v2[i] != 0: c2 += 1
+		if v1[i] != 0 and v2[i] != 0: shr += 1
+	return 1.0 - (float(shr) / (c1 + c2 - shr))
+	#return 1.0 - len(set(v1) & set(v2)) / len(set(v1) | set(v2))
+
+def ReadUserName(fname):
+	lines = [line for line in file(fname)]
+	name = {}
+	for line in lines:
+		p = line.strip().split("\t")
+		name[p[1]] = p[0]
+	return name 
+
+def ReadUserTag(fname):
+	lines = [line for line in file(fname)]
+	uid = []
+	data = []
+	for line in lines:
+		p = line.strip().split("\t")
+		# user has no tag
+		if len(p) < 2: continue
+		uid.append(p[0])
+		data.append(p[1].split(","))
+	return uid, data
+
+def ReadMatrix(fname):
+	lines = [line for line in file(fname)]
+	col_names = lines[0].strip().split("\t")
+	row_names = []
+	data = []
+	for line in lines[1:]:
+		p = line.strip().split("\t")
+		row_names.append(p[0])
+		data.append([float(x) for x in p[1:]])
+	return row_names, col_names, data
+
 if __name__ == "__main__":
-	#run
+	#uid, dat = ReadUserTag("data/user_tag_zuojie.txt")
+	#user = ReadUserName("data/user_zuojie.txt")
+	labels, people, dat = ReadMatrix("data/user_tag_matrix.txt")
+	clust = Hcluster(dat, Tanimoto)
+	DrawDendrogram(clust, labels, jpeg = "weibo_clusters.jpg")
